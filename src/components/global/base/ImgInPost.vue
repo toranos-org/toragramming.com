75f3c3be3eae195d905566de0322b61543eba888
TigRig <tigrig.crex@gmail.com>
2021-02-19 20:11:28 +0900

    feat: add global/ImgInPost
    post 内で画像を表示するため、下記要件をまとめたコンポーネント
    
    - nuxt-img を使う
    - post の path に応じた画像配置ディレクトリ（`static/assets/` 以下）を img.src のプレフィックスとして自動設定

---
+++ src/components/global/base/ImgInPost.vue
@@ -0,0 +1,24 @@
+<template>
+  <nuxt-img :src="`${dir}/${src}`" :alt="alt"></nuxt-img>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    dir() {
+      return `/assets${this.$route.path.toString()}`
+    },
+  },
+})
+</script>
